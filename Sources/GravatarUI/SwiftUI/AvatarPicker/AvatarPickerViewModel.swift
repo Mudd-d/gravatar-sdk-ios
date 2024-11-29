77d856976737514c248e0b91fa584cd58fdd7e96
Eduardo Toledo <eduardo.toledo@automattic.com>
2024-11-29 15:05:28 +0100

    Adding tests to avatar deletion flow

--- Sources/GravatarUI/SwiftUI/AvatarPicker/AvatarPickerViewModel.swift
+++ Sources/GravatarUI/SwiftUI/AvatarPicker/AvatarPickerViewModel.swift
@@ -343,9 +343,11 @@ class AvatarPickerViewModel: ObservableObject {
             selectedAvatarURL = grid.selectedAvatar?.url
         }
         let previouslySelectedAvatar = grid.selectedAvatar
-        let deletedIndex = withAnimation {
+
+        guard let deletedIndex = withAnimation(nil, {
             grid.deleteModel(avatar.id)
-        }
+        }) else { return false }
+
         if selectedAvatarURL != grid.selectedAvatar?.url {
             selectedAvatarURL = grid.selectedAvatar?.url
         }
@@ -368,7 +370,7 @@ class AvatarPickerViewModel: ObservableObject {
             try await avatarService.delete(avatarID: avatar.id, accessToken: token)
             return true
         } catch APIError.responseError(let reason) where reason.httpStatusCode == 404 {
-            return false // no-op. We delete a not-found avatar from the UI.
+            return true // no-op. We delete a not-found avatar from the UI.
         } catch APIError.responseError(reason: let reason) where reason.urlSessionErrorLocalizedDescription != nil {
             handleError(message: reason.urlSessionErrorLocalizedDescription ?? Localized.avatarDeletionError)
         } catch {
