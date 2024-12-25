cfdc6b18a5780847695d86fa665308b234023989
Pinar Olguc <pinar.olguc@automattic.com>
2024-12-25 12:48:21 +0300

    Add update tests

--- Tests/GravatarUITests/AvatarPickerProfileViewTests.swift
+++ Tests/GravatarUITests/AvatarPickerProfileViewTests.swift
@@ -4,6 +4,8 @@ import SwiftUI
 import XCTest
 
 final class AvatarPickerProfileViewTests: XCTestCase {
+    private static let width: CGFloat = 320
+
     override func invokeTest() {
         withSnapshotTesting(record: .failed) {
             super.invokeTest()
@@ -15,19 +17,45 @@ final class AvatarPickerProfileViewTests: XCTestCase {
         let testView =
             VStack(alignment: .leading, content: {
                 AvatarPickerProfileView(
-                    avatarURL: .constant(nil),
+                    avatarID: .constant(.email("")),
+                    forceRefreshAvatar: .constant(false),
                     model: .constant(
                         .init(
                             displayName: "Shelly Kimbrough",
                             location: "San Antonio, TX",
-                            profileURL: URL(string: "https://gravatar.com")
+                            profileURL: URL(string: "https://gravatar.com"),
+                            pronunciation: "SHEL-ee",
+                            pronouns: "she/her"
                         )
                     ),
                     isLoading: .constant(false)
                 )
                 .frame(maxWidth: .infinity, alignment: .leading)
             })
-            .frame(minWidth: 300)
+            .frame(minWidth: Self.width)
+
+        assertSnapshots(
+            of: testView,
+            as: [
+                .testStrategy(userInterfaceStyle: .light, layout: .sizeThatFits),
+                .testStrategy(userInterfaceStyle: .dark, layout: .sizeThatFits),
+            ]
+        )
+    }
+
+    @MainActor
+    func testAvatarPickerProfileViewEmpty() throws {
+        let testView =
+            VStack(alignment: .leading, content: {
+                AvatarPickerProfileView(
+                    avatarID: .constant(.email("")),
+                    forceRefreshAvatar: .constant(false),
+                    model: .constant(nil),
+                    isLoading: .constant(false)
+                )
+                .frame(maxWidth: .infinity, alignment: .leading)
+            })
+            .frame(minWidth: Self.width)
 
         assertSnapshots(
             of: testView,
